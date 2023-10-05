c27cb0b17c92e09d8aa7fa22bc35716bccf023f8
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-05 18:05:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers=function(drivers){
+    return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  }
+
+ const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+ 
+ const createFareMultiplier = function(value){
+
+    return function(fare){
+        return value*fare;
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+ function selectDifferentDrivers(Ham, Doc){
+    return Doc(Ham);
+
+
+ }
